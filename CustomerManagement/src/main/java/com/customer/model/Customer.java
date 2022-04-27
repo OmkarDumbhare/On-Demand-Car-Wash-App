7c2a8532d5134ba58b8ae3cacff6c114cd5cf70f
OmkarDumbhare <97183988+OmkarDumbhare@users.noreply.github.com>
2022-04-27 12:58:25 +0530

    Add files via upload

---
+++ CustomerManagement/src/main/java/com/customer/model/Customer.java
@@ -0,0 +1,114 @@
+package com.customer.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+@Document(collection = "customer")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+	@Transient
+	public static final String SEQUENCE_NAME = "users_sequence";
+
+	@Id
+	private int id;
+	private String username;
+	private String password;
+	private String name;
+	private String email;
+	private String phone;
+    private String address;
+	private boolean enabled=true;
+	private String profile;
+	private String role;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getProfile() {
+		return profile;
+	}
+
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
